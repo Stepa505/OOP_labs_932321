01b452da7a91d8b41d42650e709a0bfa79d92268
stupa <stupa@DESKTOP-J89NNH6.local>
2024-09-12 14:33:55 +0300

    Объявление методов класса не включая потокового ввода и вывода

---
+++ Array/Array/Array.h
@@ -0,0 +1,31 @@
+#pragma once
+
+template <typename ItemType>
+class Array
+{
+	Array();
+	~Array();
+	Array(const Array& arr);
+	Array(const ItemType* arr);
+	int Size() const;
+	void Swap(Array& other);
+	int FindValueFirst(ItemType& value);
+	void Sort();
+	bool InsertIndex(int index);
+	bool DeleteIndex(int index);
+	bool DeleteValue(ItemType& value);
+	bool DeleteAllValue(ItemType& value);
+	ItemType Max();
+	ItemType Min();
+	ItemType& operator [](const int index);
+	Array operator =(const Array& other);
+	Array operator +(const Array& other) const;
+	Array operator +(const ItemType& value) const;
+	Array& operator +=(const Array& other);
+	Array& operator +=(const ItemType& value);
+	bool operator !=(const Array& arr) const;
+	bool operator ==(const Array& arr) const;
+
+
+};
+
